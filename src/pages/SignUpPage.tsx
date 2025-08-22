1ca3832f29dd970d1153e7385e96a3e18fa36af1
joelOfTheSharingan <joelmgeorge.2010@icloud.com>
2025-08-22 20:21:23 +0530

    added link redirect

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -94,12 +94,12 @@ const SignUpPage: React.FC = () => {
 
         <p className="mt-4 text-sm text-center text-gray-600">
   Already have an account?{' '}
-  <a
-    href="https://joelofthesharingan.github.io/meenachil_app/#/login"
-    className="text-blue-600 hover:underline"
-  >
-    Login
-  </a>
+  <Link
+  to="/login"
+  className="text-blue-600 hover:underline"
+>
+  Login
+</Link>
 </p>
       </div>
     </div>
